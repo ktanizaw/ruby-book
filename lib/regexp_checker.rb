39700a9793a948bdb16bf079929fd8af5a5c5722
tanizawa_kenta <kt10025102@gmail.com>
2020-02-04 21:29:38 +0900

    例外処理プログラム

---
+++ lib/regexp_checker.rb
@@ -0,0 +1,15 @@
+print 'Text?:'
+test = gets.chomp
+print "Text?:"
+text = gets.chomp
+print 'Pattern?:'
+pattern = gets.chomp
+
+regexp = Regexp.new(pattern)
+matches = text.scan(regexp)
+
+if matches.size > 0
+  puts "Matched: #{matches.join(',')}"
+else
+  puts "Nothing matched."
+end
