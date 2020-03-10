6cfcc2c79826924dd674a660f866c83c8cfef8e5
tanizawa_kenta <kt10025102@gmail.com>
2020-03-10 23:40:19 +0900

    paiza

---
+++ practice/kadomatsu.rb
@@ -0,0 +1,10 @@
+def kadomatsu(short)
+    return unless short.integer?
+    return unless short.between?(1, 100)
+    middle = short/ 3 * 5
+    long = short / 3 * 7
+    puts short + middle + long
+end
+
+short = gets.to_i
+kadomatsu(short)
