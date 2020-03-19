209e3990db4c45295fbb5e4fbd1affae4fcd6b57
tanizawa_kenta <kt10025102@gmail.com>
2020-03-20 00:00:54 +0900

    paiza

--- practice/delimiter.rb
+++ practice/delimiter.rb
@@ -1,6 +1,10 @@
-
 def delimiter(string)
+  return unless string.length.between?(1, 100)
     if string.split("").count("_") < string.split("").count("-")
+        puts string.sub(/_/, '-')
+    elsif string.split("").count("_") >= string.split("").count("-")
+        puts string.sub(/-/, '_')
+    end
 end
 
 string = gets.chomp
