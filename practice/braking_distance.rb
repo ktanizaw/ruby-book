304d5560a7d721401b9a9a42f90224f48054843e
Kenta Tanizawa <kt10025102@gmail.com>
2020-03-25 11:19:18 +0900

    paiza

--- practice/braking_distance.rb
+++ practice/braking_distance.rb
@@ -1,8 +1,8 @@
 def braking_distance(*array)
-result = array[0] * array[1]^2 / (2 * array[2])
-puts result
+    return unless array[0].between?(1, 1000) || array[1].between?(1, 100) || array[2].between?(1, 10000)
+    result = array[0] * (array[1] ** 2) / (2 * array[2])
+    puts result
 end
 
 array = gets.split(" ").map!(&:to_i)
 braking_distance(*array)
-binding.irb
